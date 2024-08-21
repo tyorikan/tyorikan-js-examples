e5c798df0f45f03ab7b15c92a111a9a6345905dd
Takayuki Yorikane <tyorikan@gmail.com>
2024-08-21 15:59:26 +0900

    Fix spaces & Add MOUNT_PATH env variables (optional)

--- 99-examples/genkit-flows-integration/src/index.ts
+++ 99-examples/genkit-flows-integration/src/index.ts
@@ -16,118 +16,121 @@ import path from "path";
 
 // Configuration for indexing menu data
 const indexConfig = {
-    collection: "menuInfo",
-    contentField: "description",
-    vectorField: "embedding",
-    embedder: textEmbeddingGecko,
+  collection: "menuInfo",
+  contentField: "description",
+  vectorField: "embedding",
+  embedder: textEmbeddingGecko,
 };
 
 // Initialize Firebase Admin SDK
 const app = initializeApp({
-    credential: applicationDefault()
+  credential: applicationDefault()
 });
 const firestore = getFirestore(app);
 
 // Define Firestore retriever for menu QA
 const menuQARef = defineFirestoreRetriever({
-    name: "menuRetrieverRef",
-    firestore: firestore,
-    collection: "menuInfo",
-    contentField: "description",
-    vectorField: "embedding",
-    embedder: textEmbeddingGecko,
-    distanceMeasure: "COSINE",
+  name: "menuRetrieverRef",
+  firestore: firestore,
+  collection: "menuInfo",
+  contentField: "description",
+  vectorField: "embedding",
+  embedder: textEmbeddingGecko,
+  distanceMeasure: "COSINE",
 });
 
 // Configure Genkit with plugins and logging
 configureGenkit({
-    plugins: [
-        googleCloud(),
-        vertexAI({ location: 'asia-northeast1' }),
-    ],
-    logLevel: 'warn',
-    enableTracingAndMetrics: true,
+  plugins: [
+    googleCloud(),
+    vertexAI({ location: 'asia-northeast1' }),
+  ],
+  logLevel: 'warn',
+  enableTracingAndMetrics: true,
 });
 
 // Define flow for generating menu suggestions
 export const menuSuggestionFlow = defineFlow(
-    {
-        name: 'menuSuggestionFlow',
-        inputSchema: z.string(),
-        outputSchema: z.any(),
-    },
-    async (subject) => {
-        if (!subject) {
-            throw new Error("Input string is required.")
-        }
-        const llmResponse = await generate({
-            prompt: `${subject}をテーマにしたレストランのメニューを提案して`,
-            model: gemini15Flash,
-            config: {
-                temperature: 1,
-            },
-            output: {
-                format: 'json',
-                schema: z.object({
-                    restaurant_name: z.string(),
-                    restaurant_concept: z.string(),
-                    menus: z.array(z.object({
-                        category: z.enum(['前菜・一品料理', 'メイン料理', 'ご飯もの・麺類', 'デザート', 'ドリンク']),
-                        name: z.string(),
-                        description: z.string(),
-                        price: z.number(),
-                    }))
-                    // .describe('少なくとも 20 品以上のメニューを考えて')
-                })
-            }
-        });
-        return llmResponse.output();
+  {
+    name: 'menuSuggestionFlow',
+    inputSchema: z.string(),
+    outputSchema: z.any(),
+  },
+  async (subject) => {
+    if (!subject) {
+      throw new Error("Input string is required.")
     }
+    const llmResponse = await generate({
+      prompt: `${subject}をテーマにしたレストランのメニューを提案して`,
+      model: gemini15Flash,
+      config: {
+        temperature: 1,
+      },
+      output: {
+        format: 'json',
+        schema: z.object({
+          restaurant_name: z.string(),
+          restaurant_concept: z.string(),
+          menus: z.array(z.object({
+            category: z.enum(['前菜・一品料理', 'メイン料理', 'ご飯もの・麺類', 'デザート', 'ドリンク']),
+            name: z.string(),
+            description: z.string(),
+            price: z.number(),
+          }))
+          // .describe('少なくとも 20 品以上のメニューを考えて')
+        })
+      }
+    });
+    return llmResponse.output();
+  }
 );
 
 // Define flow for indexing menu data from a PDF file
 export const indexMenu = defineFlow(
-    {
-        name: "indexMenu",
-        inputSchema: z.string().describe("PDF file path"),
-        outputSchema: z.void(),
-    },
-    async (filePath: string) => {
-        filePath = path.resolve(filePath);
-
-        // Extract text from PDF
-        const pdfTxt = await run("extract-text", () => extractTextFromPdf(filePath));
-
-        // Chunk the text into segments
-        const chunks = await run("chunk-it", async () => chunk(pdfTxt));
-
-        // Index the chunks into Firestore
-        await run("index-chunks", async () => indexToFirestore(chunks));
+  {
+    name: "indexMenu",
+    inputSchema: z.string().describe("PDF file path"),
+    outputSchema: z.void(),
+  },
+  async (filePath: string) => {
+    if (!!process.env.MOUNT_PATH) {
+      filePath = path.join(process.env.MOUNT_PATH, filePath);
+      filePath = path.resolve(filePath);
     }
+
+    // Extract text from PDF
+    const pdfTxt = await run("extract-text", () => extractTextFromPdf(filePath));
+
+    // Chunk the text into segments
+    const chunks = await run("chunk-it", async () => chunk(pdfTxt));
+
+    // Index the chunks into Firestore
+    await run("index-chunks", async () => indexToFirestore(chunks));
+  }
 );
 
 // Define flow for answering questions about the menu
 export const menuQA = defineFlow(
-    {
-        name: 'menuQA',
-        inputSchema: z.string(),
-        outputSchema: z.string()
-    },
-    async (input: string) => {
-        // Retrieve relevant documents from Firestore
-        const docs = await retrieve({
-            retriever: menuQARef,
-            query: input,
-            options: {
-                k: 3,
-                limit: 20,
-            },
-        });
+  {
+    name: 'menuQA',
+    inputSchema: z.string(),
+    outputSchema: z.string()
+  },
+  async (input: string) => {
+    // Retrieve relevant documents from Firestore
+    const docs = await retrieve({
+      retriever: menuQARef,
+      query: input,
+      options: {
+        k: 3,
+        limit: 20,
+      },
+    });
 
-        // Generate a response using the retrieved documents
-        const llmResponse = await generate({
-            model: gemini15Flash,
-            prompt: `
+    // Generate a response using the retrieved documents
+    const llmResponse = await generate({
+      model: gemini15Flash,
+      prompt: `
       あなたは、レストランのメニューにある食べ物についての質問に答えることができる、役に立つ AI アシスタントとして行動します。
       質問に答えるのに、提供されたコンテキストのみを使用してください。
       わからない場合は、答えをでっち上げないでください。
@@ -135,34 +138,34 @@ export const menuQA = defineFlow(
   
       Question: ${input}
       `,
-            context: docs,
-        });
+      context: docs,
+    });
 
-        const output = llmResponse.text();
-        return output;
-    }
+    const output = llmResponse.text();
+    return output;
+  }
 );
 
 // Function to index text chunks into Firestore
 async function indexToFirestore(data: string[]) {
-    for (const text of data) {
-        const embedding = await embed({
-            embedder: indexConfig.embedder,
-            content: text,
-        });
-        await firestore.collection(indexConfig.collection).add({
-            [indexConfig.vectorField]: FieldValue.vector(embedding),
-            [indexConfig.contentField]: text,
-        });
-    }
+  for (const text of data) {
+    const embedding = await embed({
+      embedder: indexConfig.embedder,
+      content: text,
+    });
+    await firestore.collection(indexConfig.collection).add({
+      [indexConfig.vectorField]: FieldValue.vector(embedding),
+      [indexConfig.contentField]: text,
+    });
+  }
 }
 
 // Function to extract text from a PDF file
 async function extractTextFromPdf(filePath: string) {
-    const pdfFile = path.resolve(filePath);
-    const dataBuffer = await readFile(pdfFile);
-    const data = await pdf(dataBuffer);
-    return data.text;
+  const pdfFile = path.resolve(filePath);
+  const dataBuffer = await readFile(pdfFile);
+  const data = await pdf(dataBuffer);
+  return data.text;
 }
 
 // Start the Genkit flows server
